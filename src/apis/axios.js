ef238b6dbd4e8200336648aa02e14f0bf6453295
Alain Gomez <alain.gomez@cargonerds.com>
2023-03-29 10:33:08 +0200

    Videothek

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -5,6 +5,10 @@ import axios from "axios";
 // })
 
 export default axios.create({
-    baseURL: "https://localhost:7006/api/"
+    baseURL: `http://${process.env.REACT_APP_API_URL}/api/`,
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
+        'Access-Control-Allow-Credentials': true
+    }
 });
-
